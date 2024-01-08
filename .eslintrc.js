eef30d5f5f5bc891dfeefaa6f74bc025308a73ff
harley <a1209082835@gmail.com>
2024-01-08 17:56:29 +0800

    🌈 style: eslint风格修改

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,4 +4,13 @@ module.exports = {
     page: true,
     REACT_APP_ENV: true,
   },
+  rules: {
+    'no-console': 'off',
+    'no-unused-vars': 'off',
+    'no-underscore-dangle': 'off',
+    'no-param-reassign': 'off',
+    'no-plusplus': 'off',
+    'no-continue': 'off',
+    'no-restricted-syntax': 'off',
+  },
 };
