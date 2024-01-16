97aef08b7be7bcfe66f8ebac09a971276c904fbb
harley <a1209082835@gmail.com>
2024-01-16 22:17:04 +0800

    1

--- config/config.ts
+++ config/config.ts
@@ -10,8 +10,9 @@ const { REACT_APP_ENV = 'dev' } = process.env;
 export default defineConfig({
   /**
    * @name define 部署到非根目录时需要配置
-  */ 
+   */
   base: '/admin/',
+  publicPath: '/admin/',
   /**
    * @name 开启 hash 模式
    * @description 让 build 之后的产物包含 hash 后缀。通常用于增量发布和避免浏览器加载缓存。
