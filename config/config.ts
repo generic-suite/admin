510ebb0384dd3c4cc19e838f4fee22d0d433ed3c
harley <a1209082835@gmail.com>
2024-01-16 13:54:42 +0800

    部署到非根目录

--- config/config.ts
+++ config/config.ts
@@ -9,6 +9,11 @@ const { REACT_APP_ENV = 'dev' } = process.env;
 
 export default defineConfig({
   /**
+   * @name define 部署到非根目录时需要配置
+  */ 
+  base: '/admin/',
+  publicPath: '/admin/',
+  /**
    * @name 开启 hash 模式
    * @description 让 build 之后的产物包含 hash 后缀。通常用于增量发布和避免浏览器加载缓存。
    * @doc https://umijs.org/docs/api/config#hash
