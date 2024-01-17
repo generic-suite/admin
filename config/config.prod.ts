a1e5c1dcd7ef6c60fbfd46508a9d64f53206a58f
harley <a1209082835@gmail.com>
2024-01-17 11:13:23 +0800

    修改环境变量

--- config/config.prod.ts
+++ config/config.prod.ts
@@ -3,7 +3,7 @@ import { defineConfig } from '@umijs/max';
 export default defineConfig({
   define: {
     REACT_APP_ENV: 'production',
-    REACT_APP_BASE_URL: 'http://localhost:10303/api',
-    REACT_APP_FILE_URL: 'http://localhost:10303/api',
+    REACT_APP_BASE_URL: '/api',
+    REACT_APP_FILE_URL: '',
   },
 });
